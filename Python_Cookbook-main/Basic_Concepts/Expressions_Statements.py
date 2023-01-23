aa4e0d6290f4f0df9502bc4ae07a7510c1d5fe09
Efe Bütüner <72016793+efebutuner@users.noreply.github.com>
2023-01-23 11:12:41 +0000

    v1

---
+++ Python_Cookbook-main/Basic_Concepts/Expressions_Statements.py
@@ -0,0 +1,3 @@
+iq = 100
+user_age = iq / 5 # iq / 5 is an expression which generates a number
+                  #user_age = iq / 5 is a statement
---
+++ Python_Cookbook-main/Basic_Concepts/Variables.py
@@ -0,0 +1,14 @@
+iq = 190
+print(iq)
+
+#Variables are usually named with snake_case, which means they should be starting with lowercases or underscores
+#Variables are also case sensitive which means we should watch out the uppercase or lowercase characters to be matched while recalling our variables
+
+user_iq = 190
+user_age = user_iq / 4
+print(user_age)
+
+# constants: variables that are assigned using all Upercase characters that means this value should not be changed within the code.
+PI = 3.14
+
+a,b,c = 1, 2, 3 # We can make rapid assignments to variables using commas.
