5f6888b142708550eea78514d71a2c29c913bfa4
Efe Bütüner <72016793+efebutuner@users.noreply.github.com>
2023-01-23 10:19:03 +0000

    v1

---
+++ Python_Cookbook-main/Basic_Concepts/Python_Data_Types.py
@@ -0,0 +1,27 @@
+#Fundamental Data Types
+
+"""
+int 
+float
+str
+bool
+list
+tuple
+set
+dict
+"""
+
+#Classes -> custom types
+
+#Specialized Data Types
+"""
+None
+"""
+
+#Numbers (int and float)
+print(2 + 4)
+print(2 - 4)
+print(2 * 4)
+print(2 / 4)
+
+print(type(2 + 4)) # Type function returns the data type of the variable or the statement inside its brackets
