6328fa689341872ffec15a4ebe7701723655cd26
Efe Bütüner <72016793+efebutuner@users.noreply.github.com>
2023-01-23 10:31:47 +0000

    v1

--- Python_Cookbook-main/Basic_Concepts/Python_Data_Types.py
+++ Python_Cookbook-main/Basic_Concepts/Python_Data_Types.py
@@ -13,15 +13,21 @@ dict
 
 #Classes -> custom types
 
-#Specialized Data Types
-"""
-None
-"""
+#Specialized Data Types (None)
 
 #Numbers (int and float)
 print(2 + 4)
 print(2 - 4)
 print(2 * 4)
-print(2 / 4)
+print(2 / 4) # = float as 2 divided by 4 is 0.5
 
 print(type(2 + 4)) # Type function returns the data type of the variable or the statement inside its brackets
+print(type(10.56)) # Floating point numbers are stored in 2 different places inside the memory where first location stores the numbers before dot and second point stores the numbers after the dot
+
+print(2 ** 2) # Two to the power of two
+print(10 // 4) # Remainder of divison
+print(5 % 4) # Remainder of division
+
+# Math Functions
+print(round(3.1))
+print(abs(-5))
