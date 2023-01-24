bd20862e78822a9ffd2f2998b25ea1ace32ef886
Efe Bütüner <72016793+efebutuner@users.noreply.github.com>
2023-01-24 07:00:21 +0000

    v1

---
+++ Python_Cookbook-main/Lists_Matrix_Dictionaries/Dictionaries.py
@@ -0,0 +1,23 @@
+#Dictionary
+dictionary = {
+    'a' : 1,
+    'b' : 2
+}
+
+print(dictionary['b'])  # Dictionary structure takes the input as key and returns the value assigned to that key
+print(dictionary)
+
+my_list = [
+    {
+    'a': [1,2,3],
+    'b': 'Hello',
+    'x': True
+    },
+    {
+    'a': [4,5,6],
+    'b': 'hello',
+    'x': True
+    }
+]
+
+print(my_list[0]['a'][2])
