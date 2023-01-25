ac6056af8bba77f9af914f7d28eebb111a5620f4
Efe Bütüner <72016793+efebutuner@users.noreply.github.com>
2023-01-25 14:17:53 +0000

    v1

---
+++ Python_Cookbook-main/Functions/Methods_and_Functions.py
@@ -0,0 +1,4 @@
+#Methods and Functions
+
+#Methods are coming with objects as built-in.
+#We can access to the methods of objects by putting a dot "." behind the object and see built in methods for that object that we can use.
