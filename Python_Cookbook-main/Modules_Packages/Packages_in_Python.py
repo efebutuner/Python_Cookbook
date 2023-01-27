ff241694537f1a2331d6dc9a444076401fb5534f
Efe Bütüner <72016793+efebutuner@users.noreply.github.com>
2023-01-27 14:36:36 +0000

    v1

--- Python_Cookbook-main/Modules_Packages/Packages_in_Python.py
+++ Python_Cookbook-main/Modules_Packages/Packages_in_Python.py
@@ -1,3 +1,4 @@
 #Packages in Python
 #packages are imported via ==> import module_name.package_name
 # Any time we are trying to create a package, we should have the __init__.py package inside that module for Python to be able to generate that package
+#from package_name.module_name import funciton_name
