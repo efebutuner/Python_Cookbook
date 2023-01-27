9822240d15d2ab015c37652679083601b02871a6
Efe Bütüner <72016793+efebutuner@users.noreply.github.com>
2023-01-27 08:18:33 +0000

    v1

--- Python_Cookbook-main/Functions/Set_Comprehensions.py
+++ Python_Cookbook-main/Functions/Set_Comprehensions.py
@@ -1,1 +1,16 @@
-#lsi
+#list is done by [] 
+#set is done by {}
+
+my_set = {num for num in range(0,100)}
+print(my_set)
+
+#dict comprehension
+
+simple_dict = {
+    'a': 1,
+    'b': 2
+}
+
+my_dict = {key:value**2 for key, value in simple_dict.items() if value % 2 == 0}
+my_dictt = {num: num*2 for num in [1,2,3]}
+print(my_dict)
