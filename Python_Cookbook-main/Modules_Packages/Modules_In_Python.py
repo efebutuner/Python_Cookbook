094386205a3022c047a560118ad8a2d4d93dd16b
Efe Bütüner <72016793+efebutuner@users.noreply.github.com>
2023-01-27 14:00:44 +0000

    v1

---
+++ Python_Cookbook-main/Modules_Packages/Modules_In_Python.py
@@ -0,0 +1,10 @@
+# Each .py files are modules of python
+# We can make our own modules
+# Modules are like classes, we have modules dedicated to some actions like ,logging or plotting some sort of graps or utilities.
+
+#utility.py
+#def xxxxx:
+#   xxxxx
+
+#main.py
+#import utility
---
+++ Python_Cookbook-main/Modules_Packages/Packages_in_Python.py
@@ -0,0 +1,3 @@
+#Packages in Python
+#packages are imported via ==> import module_name.package_name
+# Any time we are trying to create a package, we should have the __init__.py package inside that module for Python to be able to generate that package
